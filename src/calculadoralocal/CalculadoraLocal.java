bfeb030f59bcfd836b19e5671d9373d8498ee3d0
Thiago Krug <thiagockrug@gmail.com>
2012-09-16 21:28:59 -0300

    first commit

---
+++ src/calculadoralocal/CalculadoraLocal.java
@@ -0,0 +1,15 @@
+package calculadoralocal;
+
+/**
+ *
+ * @author thiago
+ */
+public class CalculadoraLocal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
