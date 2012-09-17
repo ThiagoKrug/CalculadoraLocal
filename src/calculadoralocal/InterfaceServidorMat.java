94f62df26d5e6be2f8bed6bb4a8959c318013e4d
= RafhaelRC <= rcrafhaelrc@gmail.com>
2012-09-16 22:09:36 -0300

    implementação inicial

---
+++ src/calculadoralocal/InterfaceServidorMat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoralocal;
+
+/**
+ *
+ * @author RafhaelRC
+ */
+public interface InterfaceServidorMat extends Remote
+{
+    public double soma(double a, double b) throws RemoteException;
+    public double subtrai(double a, double b) throws RemoteException;
+    public double multiplica(double a, double b) throws RemoteException;
+    public double divide(double a, double b) throws RemoteException;
+} 
---
+++ src/calculadoralocal/ServidorMat.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoralocal;
+
+/**
+ *
+ * @author RafhaelRC
+ */
+public class ServidorMat extends UnicastRemoteObject implements InterfaceServidorMat
+{
+    public ServidorMat() throws RemoteException
+    {
+        System.out.println("Novo Servidor instanciado...");
+    }
+    public double soma(double a, double b) throws RemoteException
+    {
+        return a+b;
+    }
+    public double subtrai(double a, double b) throws RemoteException
+    {
+        return a-b;
+    }
+    public double multiplica(double a, double b) throws RemoteException
+    {
+        return a*b;
+    }
+    public double divide(double a, double b) throws RemoteException
+    {
+        return a/b;
+    }
+} 
